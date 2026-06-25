218d1203fa8fbbe08ca2cafda14994f8a243cc89
elliot <945962873@qq.com>
2026-06-25 14:08:04 +0800

    fix light theme color issues

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,8 +1,13 @@
+/// <reference types="vite/client" />
+
 import DefaultTheme from 'vitepress/theme'
 import Layout from './Layout.vue'
 import PricingFeatureSystem from './components/PricingFeatureSystem.vue'
 import { type Theme } from 'vitepress'
 import './custom.css'
+
+// @ts-ignore - Tells TypeScript to ignore the missing type definitions for this specific plugin
+import googleAnalytics from 'vitepress-plugin-google-analytics'
 
 const theme: Theme = {
   extends: DefaultTheme,
@@ -10,6 +15,13 @@ const theme: Theme = {
 
   enhanceApp({ app }) {
     app.component('PricingFeatureSystem', PricingFeatureSystem)
+    
+    // Only trigger tracking when the site is built and running in production
+    if (import.meta.env.PROD) {
+      googleAnalytics({
+        id: 'G-JML9VGDMXJ', // Replace with your real ID
+      })
+    }
   }
 }
 
